0059eed1294174824e9a35f2635b515132167595
user.name <goosewood@protonmail.com>
2018-02-18 16:47:25 -0500

    commented

--- recipies.py
+++ recipies.py
@@ -6,6 +6,7 @@ class recipies(object):
     '''
     def __init__(self, names, num):
         '''
+        takes names (strings in list), and number of recipies to be returned
         '''
         "%2C".join(names)
         self.response = unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients="+str(names)+"&limitLicense=false&number="+str(num)+"&ranking=1",
